9650ba1b142bf48080d95786618d8e44de98582d
vellasamy <venkadeshl2022@gmail.com>
2023-03-28 20:16:55 +0530

    update

---
+++ reduce sum-6.js
@@ -0,0 +1,9 @@
+
+                //sum-6 reduce method//
+						 const numbers = [1, 2, 3, 4, 5];
+						  const mul = numbers.reduce(function (v1, v2) {
+						  quiz= v1 * v2;
+								  return quiz;
+								});
+							console.log(mul/3);
+							document.write("<br>",mul/3);
